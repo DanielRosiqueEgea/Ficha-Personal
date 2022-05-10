be37bb9cbeb32c701317a43c1e103a44b1b20651
Roxoner44 <58725397+Roxoner44@users.noreply.github.com>
2022-05-10 11:16:21 +0200

    Add files via upload

---
+++ FichaPersonal/FichaPersonal/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace FichaPersonal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFichaPers = new System.Windows.Forms.Label();
+            this.lblCiudad = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblFoto = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cbCiudad = new System.Windows.Forms.ComboBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.opfFoto = new System.Windows.Forms.OpenFileDialog();
+            this.gbInsertDatos = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.gbInsertDatos.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFichaPers
+            // 
+            this.lblFichaPers.AutoSize = true;
+            this.lblFichaPers.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFichaPers.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblFichaPers.Location = new System.Drawing.Point(12, 23);
+            this.lblFichaPers.Name = "lblFichaPers";
+            this.lblFichaPers.Size = new System.Drawing.Size(174, 29);
+            this.lblFichaPers.TabIndex = 0;
+            this.lblFichaPers.Text = "Ficha Personal";
+            // 
+            // lblCiudad
+            // 
+            this.lblCiudad.AutoSize = true;
+            this.lblCiudad.Location = new System.Drawing.Point(9, 118);
+            this.lblCiudad.Name = "lblCiudad";
+            this.lblCiudad.Size = new System.Drawing.Size(52, 17);
+            this.lblCiudad.TabIndex = 1;
+            this.lblCiudad.Text = "Ciudad";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(9, 64);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(67, 17);
+            this.lblDireccion.TabIndex = 2;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(9, 13);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.AutoSize = true;
+            this.lblFoto.Location = new System.Drawing.Point(58, 18);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(36, 17);
+            this.lblFoto.TabIndex = 4;
+            this.lblFoto.Text = "Foto";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(25, 84);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(169, 22);
+            this.txtDireccion.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNombre.Location = new System.Drawing.Point(25, 33);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 6;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // cbCiudad
+            // 
+            this.cbCiudad.FormattingEnabled = true;
+            this.cbCiudad.Items.AddRange(new object[] {
+            "Murcia",
+            "Barcelona",
+            "Madrid",
+            "Valencia"});
+            this.cbCiudad.Location = new System.Drawing.Point(25, 138);
+            this.cbCiudad.Name = "cbCiudad";
+            this.cbCiudad.Size = new System.Drawing.Size(121, 24);
+            this.cbCiudad.TabIndex = 7;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnValidar.Location = new System.Drawing.Point(483, 134);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar.TabIndex = 8;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInsertar.Location = new System.Drawing.Point(61, 124);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 11;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFoto.Location = new System.Drawing.Point(61, 38);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(84, 80);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFoto.TabIndex = 12;
+            this.pbFoto.TabStop = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.Location = new System.Drawing.Point(483, 163);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Location = new System.Drawing.Point(483, 192);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // opfFoto
+            // 
+            this.opfFoto.Filter = " All|*.*|Bitmap|*.bmp|JPEG|*.jpg";
+            this.opfFoto.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // gbInsertDatos
+            // 
+            this.gbInsertDatos.Controls.Add(this.lblNombre);
+            this.gbInsertDatos.Controls.Add(this.lblCiudad);
+            this.gbInsertDatos.Controls.Add(this.lblDireccion);
+            this.gbInsertDatos.Controls.Add(this.txtDireccion);
+            this.gbInsertDatos.Controls.Add(this.txtNombre);
+            this.gbInsertDatos.Controls.Add(this.cbCiudad);
+           
+            this.gbInsertDatos.Location = new System.Drawing.Point(16, 55);
+            this.gbInsertDatos.Name = "gbInsertDatos";
+            this.gbInsertDatos.Size = new System.Drawing.Size(221, 172);
+            this.gbInsertDatos.TabIndex = 15;
+            this.gbInsertDatos.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblFoto);
+            this.groupBox1.Controls.Add(this.btnInsertar);
+            this.groupBox1.Controls.Add(this.pbFoto);
+            this.groupBox1.Location = new System.Drawing.Point(300, 259);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 177);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Foto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(657, 470);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbInsertDatos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.lblFichaPers);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.gbInsertDatos.ResumeLayout(false);
+            this.gbInsertDatos.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFichaPers;
+        private System.Windows.Forms.Label lblCiudad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblFoto;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cbCiudad;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.OpenFileDialog opfFoto;
+        private System.Windows.Forms.GroupBox gbInsertDatos;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FichaPersonal/FichaPersonal/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FichaPersonal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            if ((txtNombre.Text == "") || (txtDireccion.Text == "") || (cbCiudad.Text == ""))
+            {
+                MessageBox.Show("Ha de rellenar el formulario.");
+            }
+            else
+            {
+                MessageBox.Show("Los datos se han rellenado correctamente.");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = null;
+            txtDireccion.Text = null;
+            cbCiudad.Text = null;
+            pbFoto.Image = null;
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (opfFoto.ShowDialog() == DialogResult.OK)
+            {
+                pbFoto.Image = Image.FromFile(opfFoto.FileName);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                txtNombre.BackColor = System.Drawing.Color.Red;
+            }
+            else {
+                txtNombre.BackColor = System.Drawing.SystemColors.Window;
+                if (e.KeyChar == (char)13)
+                {
+                    txtDireccion.Focus();
+                }
+               
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
